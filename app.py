0413d29ceb69a8f031bcc21bcdbcc8a83a669030
devspectre <devspectre@yandex.com>
2020-05-15 19:17:50 +0300

    added data table

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/r')
+
+def generate_table(dataframe, max_rows=10):
+  return html.Table([
+    html.Thead(
+      html.Tr([html.Th(col) for col in dataframe.columns])
+    ),
+    html.Tbody([
+      html.Tr([html.Td(dataframe.iloc[i][col]) for col in dataframe.columns]) for i in range(min(len(dataframe), max_rows))
+    ])
+  ])
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -12,6 +25,8 @@ colors = {
 }
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+  html.H4(children='US Agriculture Exports (2011)'),
+  generate_table(df),
   html.H1(
     children='Hello Dash! I wanna a bite!',
     style={
